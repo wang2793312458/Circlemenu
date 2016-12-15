8d98d4e0404f9143786cbe87cf522336478c608f
wz2793312458 <wz2793312458@163.com>
2016-12-15 17:31:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/circlemenu/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.circlemenu;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.example.cmlibrary.CircleMenu;
+import com.example.cmlibrary.OnMenuSelectedListener;
+import com.example.cmlibrary.OnMenuStatusChangeListener;
+
+public class MainActivity extends AppCompatActivity {
+private CircleMenu circleMenu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        circleMenu= (CircleMenu) findViewById(R.id.circle_menu);
+        circleMenu.setMainMenu(Color.parseColor("#CDCDCD"), R.mipmap.icon_menu, R.mipmap.icon_cancel)
+                .addSubMenu(Color.parseColor("#258CFF"), R.mipmap.icon_home)
+                .addSubMenu(Color.parseColor("#30A400"), R.mipmap.icon_search)
+                .addSubMenu(Color.parseColor("#FF4B32"), R.mipmap.icon_notify)
+                .addSubMenu(Color.parseColor("#8A39FF"), R.mipmap.icon_setting)
+                .addSubMenu(Color.parseColor("#FF6A00"), R.mipmap.icon_gps)
+                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+                    @Override
+                    public void onMenuSelected(int index) {}
+                }).setOnMenuStatusChangeListener(new OnMenuStatusChangeListener() {
+            @Override
+            public void onMenuOpened() {
+
+            }
+
+            @Override
+            public void onMenuClosed() {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        circleMenu.openMenu();
+        return super.onMenuOpened(featureId, menu);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        circleMenu.closeMenu();
+    }
+}
